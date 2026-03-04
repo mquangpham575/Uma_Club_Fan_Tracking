b3f4d9f158b5ee6d9455a37861fa6069aeaee1d2
mquangpham575 <pmqminhquang@gmail.com>
2026-03-04 16:59:16 +0700

    feat: github action on push (main) & update: complete march data

--- config/globals.py
+++ config/globals.py
@@ -9,115 +9,101 @@ SHEET_ID = "1O09PM-hYo-H05kWWqMg71GelEpfaGrePQWzdDCKOqyU"
 CLUBS = {
     "1": {
         "title": "ENDLESS (SS)",
-        "SEARCH_TERM": "ENDLESS",
         "club_id": "145606097",
         "THRESHOLD": 4500000,
     },
     "2": {
         "title": "ENDER (SS)",
-        "SEARCH_TERM": "ENDER",
         "club_id": "971029133",
-        "THRESHOLD": 4000000,
+        "THRESHOLD": 4500000,
     },
     "3": {
         "title": "ENDCORE (S+)",
-        "SEARCH_TERM": "ENDCORE",
         "club_id": "168472480",
         "THRESHOLD": 3000000,
     },
     "4": {
-        "title": "ENDGOON (S+)",
-        "SEARCH_TERM": "ENDGOON",
+        "title": "ENDGOON (S)",
         "club_id": "125289696",
-        "THRESHOLD": 3000000,
+        "THRESHOLD": 2000000,
     },
     "5": {
         "title": "ENDGAME (S)",
-        "SEARCH_TERM": "ENDGAME",
         "club_id": "558806657",
-        "THRESHOLD": 2100000,
+        "THRESHOLD": 2000000,
     },
     "6": {
         "title": "ENDURANCE (S)",
-        "SEARCH_TERM": "ENDURANCE",
         "club_id": "237354394",
-        "THRESHOLD": 2100000,
+        "THRESHOLD": 2000000,
     },
     "7": {
         "title": "ENDURE (S)",
-        "SEARCH_TERM": "ENDURE",
         "club_id": "370447850",
-        "THRESHOLD": 1800000,
+        "THRESHOLD": 2000000,
     },
     "8": {
         "title": "ENIGMA (A+)",
-        "SEARCH_TERM": "ENIGMA",
         "club_id": "166373193",
         "THRESHOLD": 1300000,
     },
     "9": {
         "title": "ENDYMION (A+)",
-        "SEARCH_TERM": "ENDYMION",
         "club_id": "636004975",
         "THRESHOLD": 1300000,
     },
     "10": {
-        "title": "ENDDOOKIE (A)",
-        "SEARCH_TERM": "ENDDOOKIE",
-        "club_id": "532271884",
+        "title": "ENDWAVES (A+)",
+        "club_id": "815680661",
         "THRESHOLD": 1000000,
     },
     "11": {
+        "title": "ENDDOOKIE (A+)",
+        "club_id": "532271884",
+        "THRESHOLD": 1000000,
+    },
+    "12": {
         "title": "ENDFIELD (A)",
-        "SEARCH_TERM": "ENDFIELD",
         "club_id": "653291031",
         "THRESHOLD": 700000,
     },
-    "12": {
+    "13": {
         "title": "ENDMOON (A)",
-        "SEARCH_TERM": "ENDMOON",
         "club_id": "327708043",
         "THRESHOLD": 700000,
     },
-    "13": {
+    "14": {
         "title": "ENDGRAVE (Casual)",
-        "SEARCH_TERM": "ENDGRAVE",
         "club_id": "647067019",
         "THRESHOLD": 0,
     },
-    "14": {
+    "15": {
         "title": "ENDVILE (Casual)",
-        "SEARCH_TERM": "ENDVILE",
         "club_id": "114502546",
         "THRESHOLD": 0,
     },
-    "15": {
+    "16": {
         "title": "UMA Vault (S)",
-        "SEARCH_TERM": "UMA Vault",
         "club_id": "150259101",
         "THRESHOLD": 2100000,
     },
-    "16": {
+    "17": {
         "title": "Dirt Idols (A+)",
-        "SEARCH_TERM": "Dirt Idols",
         "club_id": "524890130",
         "THRESHOLD": 1000000,
     },
-    "17": {
+    "18": {
         "title": "Artisia (A+)",
-        "SEARCH_TERM": "Artisia",
         "club_id": "531543637",
         "THRESHOLD": 1000000,
     },
-    "18": {
+    "19": {
         "title": "MamboFanbo (A)",
-        "SEARCH_TERM": "MamboFanbo",
         "club_id": "394737639",
         "THRESHOLD": 700000,
     },
-    "19": {
+    "20": {
         "title": "yurimusume (Casual)",
-        "SEARCH_TERM": "yurimusume",
         "club_id": "415579817",
         "THRESHOLD": 0,
     },
